671c0edc97c17a800cf048873ce1200e73a88eb9
robert <raniszewskirobert27@gmail.com>
2025-05-28 19:59:58 +0200

    dziala na 5ms

--- fpga_/debounce.vhd
+++ fpga_/debounce.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity debounce is
     Port (
@@ -11,17 +11,20 @@ entity debounce is
 end debounce;
 
 architecture Behavioral of debounce is
-    signal counter : STD_LOGIC_VECTOR(19 downto 0) := (others => '0');
+    constant DEBOUNCE_LIMIT : unsigned(17 downto 0) := to_unsigned(16#FFFFF#, 18); 
+    signal counter : unsigned(17 downto 0) := (others => '0');
     signal btn_sync : STD_LOGIC := '0';
 begin
     process(clk)
     begin
         if rising_edge(clk) then
             if btn_in = '1' then
-                if counter = X"FFFFF" then
-                    btn_sync <= '1';
-                else
+                if counter < DEBOUNCE_LIMIT then
                     counter <= counter + 1;
+                end if;
+
+                if counter = DEBOUNCE_LIMIT then
+                    btn_sync <= '1';
                 end if;
             else
                 counter <= (others => '0');
@@ -29,5 +32,9 @@ begin
             end if;
         end if;
     end process;
+
     btn_out <= btn_sync;
 end Behavioral;
+
+
+
